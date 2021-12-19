812078769035803aa0ff0cac0e706e30d94e77f2
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-12-19 15:19:08 +0100

    Add files via upload

---
+++ formationintermediate1/formationintermediate1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace formationintermediate1
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = Person.Parse("Robin");
+            person.Introduce("Robin");
+        }
+    }
+}
