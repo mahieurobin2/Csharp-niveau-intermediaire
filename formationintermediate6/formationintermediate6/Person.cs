c2eb8c4ad8fd3085bf02a7d515d13711547dc5af
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-12-19 15:21:30 +0100

    Add files via upload

---
+++ formationintermediate6/formationintermediate6/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace formationintermediate6
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public DateTime Birthdate { get; private set; }
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+  
+
+        public int Age
+        {
+            get 
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+
+                return years;
+            }
+        }
+    }
+}
---
+++ formationintermediate6/formationintermediate6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace formationintermediate6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(2001, 12, 08));
+            Console.WriteLine(person.Age);
+        }
+    }
+}
