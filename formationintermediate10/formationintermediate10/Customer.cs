cde110db326a3a6076378e243d2c137cd19287a5
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-12-19 15:23:15 +0100

    Add files via upload

---
+++ formationintermediate10/formationintermediate10/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace formationintermediate10
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+            var calculator = new RateCalculator();
+            var rating = calculator.Calculate(this);
+            Console.WriteLine("Promoted logic changed.");
+        }
+
+        private int CalculateRating(bool excludeOrders)
+        {
+            return 0;
+        }
+    }
+}
---
+++ formationintermediate10/formationintermediate10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace formationintermediate10
+{
+    class Program
+    {
+        public class GoldCustimer: Customer
+        {
+            public void OfferVouchar()
+            {
+                var rating = this CalculateRating(excludeOrders: true);
+            }
+
+            private void CalculateRating(bool excludeOrders)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+        }
+    }
+}
