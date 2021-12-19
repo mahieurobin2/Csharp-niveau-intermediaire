a454186243a2dc1e8fc50630b48e12effddf9424
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-12-19 15:25:28 +0100

    Add files via upload

---
+++ formationintermediate12/formationintermediate12/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace formationintermediate12
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Text text = new Text();
+        //    Shape shape = text;
+
+        //    text.Width = 200;
+        //    shape.Width = 100;
+
+        //    Console.WriteLine(text.Width);
+        // }
+
+        // StreamReader reader = new StreamReader(new MemoryStream());
+        Shape shape = new Text();
+        Text text = (Text) shape;
+    }
+}
