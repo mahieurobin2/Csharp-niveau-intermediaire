542e4f4841280dadb1c74620041237c846981a46
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-12-19 15:21:03 +0100

    Add files via upload

---
+++ formationintermediate5/formationintermediate5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+public class Person
+{
+    private DateTime _birthdate;
+
+    public void SetBirthdate(DateTime birthdate)
+    {
+        _birthdate = birthdate;
+    }
+
+    public DateTime GetBirthdate()
+    {
+        return _birthdate;
+    }
+}
+namespace formationintermediate5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(2001, 12, 08));
+            Console.WriteLine(person.GetBirthdate());
+        }
+    }
+}
