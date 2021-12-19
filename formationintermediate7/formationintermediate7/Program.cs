2756dc29ef10fd40e36deadbbff9291c4e00b3f7
mahieurobin2 <92267236+mahieurobin2@users.noreply.github.com>
2021-12-19 15:21:56 +0100

    Add files via upload

---
+++ formationintermediate7/formationintermediate7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace formationintermediate7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie();
+            cookie["name"] = "Robin";
+            Console.WriteLine(cookie["name"]);
+        }
+    }
+}
